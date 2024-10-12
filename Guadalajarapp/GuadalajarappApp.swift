8360a413b9f925e8874d37c59accb93a20a70920
Daniel <bastian.moreton@gmail.com>
2024-10-11 23:22:40 -0500

    Initial Commit

---
+++ Guadalajarapp/GuadalajarappApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuadalajarappApp.swift
+//  Guadalajarapp
+//
+//  Created by Daniel Moreno on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GuadalajarappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuadalajarappTests/GuadalajarappTests.swift
@@ -0,0 +1,16 @@
+//
+//  GuadalajarappTests.swift
+//  GuadalajarappTests
+//
+//  Created by Daniel Moreno on 10/11/24.
+//
+
+import Testing
+
+struct GuadalajarappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
